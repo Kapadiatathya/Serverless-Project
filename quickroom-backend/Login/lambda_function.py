00f1c790e293e45500ec650107a1452671b1fde3
Tathya Kapadia <tt857842@dal.ca>
2024-06-23 22:39:04 -0300

    Authentication Feature Code push

---
+++ quickroom-backend/Login/lambda_function.py
@@ -0,0 +1,42 @@
+import boto3
+from botocore.exceptions import ClientError
+import json
+
+dynamodb = boto3.resource('dynamodb')
+agent_table = dynamodb.Table('agents') 
+user_table = dynamodb.Table('customers')   
+
+def lambda_handler(event, context):
+    # Parse request parameters
+    body = json.loads(event['body'])
+    email = body['email']
+    role = body['role']
+
+    try:
+        if role == 'agent':
+            response = agent_table.get_item(Key={'email': email})
+        elif role == 'customer':
+            response = user_table.get_item(Key={'email': email})
+        else:
+            return {
+                'statusCode': 400,
+                'body': json.dumps('Invalid role specified')
+            }
+
+        if 'Item' in response:
+            item = response['Item']
+            return {
+                'statusCode': 200,
+                'body': json.dumps(item)
+            }
+        else:
+            return {
+                'statusCode': 404,
+                'body': json.dumps('Item not found')
+            }
+
+    except ClientError as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(str(e))
+        }
